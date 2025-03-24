0f253796c932710bee2c2ebd26cb40016e95a8ac
Satoshi.Iseri <iserist@fsi.co.jp>
2025-03-25 08:45:56 +0900

    header非表示

--- app.py
+++ app.py
@@ -7,6 +7,54 @@ import json
 
 st.set_page_config(page_title="藤田ソフィアのチャットルーム", page_icon="💬")
 
+st.markdown("""
+<style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+				        .appview-container .main .block-container{
+                            padding-top: 1rem;
+                            padding-right: 3rem;
+                            padding-left: 3rem;
+                            padding-bottom: 1rem;
+                        }  
+                        .reportview-container {
+                            padding-top: 0rem;
+                            padding-right: 3rem;
+                            padding-left: 3rem;
+                            padding-bottom: 0rem;
+                        }
+                        header[data-testid="stHeader"] {
+                            z-index: -1;
+                        }
+                        div[data-testid="stToolbar"] {
+                        z-index: 100;
+                        }
+                        div[data-testid="stDecoration"] {
+                        z-index: 100;
+                        }
+</style>
+""", unsafe_allow_html=True)
+
 with st.sidebar:
     st.title('藤田ソフィアのチャットルーム')
 
